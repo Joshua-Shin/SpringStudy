705f4637956af3ecab0e27d59bd17ae6f24af320
sjh910805@gmail.com <sjh910805@gmail.com>
2023-01-03 20:57:40 +0900

    test: 싱글톤 빈에 프로토타입을 주입했을시 프로토타입도 계속 유지되는지 테스트

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -23,7 +23,31 @@ public class SingletonWithPrototypeTest1 {
         assertThat(prototypeBean2.getCount()).isEqualTo(1);
 
     }
+    @Test
+    void prototypeFind2() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(
+                PrototypeBean.class, ClientBean.class);
+        ClientBean bean = ac.getBean(ClientBean.class);
+        int count1 = bean.login();
+        assertThat(count1).isEqualTo(1);
 
+        ClientBean bean2 = ac.getBean(ClientBean.class);
+        int count2 = bean2.login();
+        assertThat(count2).isEqualTo(2);
+    }
+
+    static class ClientBean {
+        private final PrototypeBean prototypeBean;
+
+        public ClientBean(PrototypeBean prototypeBean) {
+            this.prototypeBean = prototypeBean;
+        }
+        public int login() {
+            prototypeBean.addCount();
+            return prototypeBean.getCount();
+        }
+
+    }
     @Scope("prototype")
     static class PrototypeBean {
         private int count = 0;
